cf3f46e865fe2d137acf776f2032e9d7049b56d9
Budgie <111358624+ChipoEllen@users.noreply.github.com>
2023-07-31 23:57:36 +0200

    Update main.py

--- main.py
+++ main.py
@@ -36,8 +36,8 @@ def check_all_messages(message):
         highest_prob_list[bot_response] = message_probability(message, list_of_words, single_response, required_words)
 
     # Responses -------------------------------------------------------------------------------------------------------
-    response('Hello!', ['hello', 'hi', 'hey', 'sup', 'heyo'], single_response=True)
-    response('See you!', ['bye', 'goodbye'], single_response=True)
+    response('Hello!', ['hello', 'hi', 'hey', 'sup', 'heyo','hallo'], single_response=True)
+    response('See you!', ['bye', 'goodbye','So long'], single_response=True)
     response('I\'m doing fine, and you?', ['how', 'are', 'you', 'doing'], required_words=['how'])
     response('You\'re welcome!', ['thank', 'thanks'], single_response=True)
     response('Thank you!', ['i', 'love', 'code', 'palace'], required_words=['code', 'palace'])
